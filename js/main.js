7efd6114721d02a3cc4110efa34c6f78dd062d41
Astra Solar <techsupport@Astras-Mac-Studio.local>
2023-05-02 15:01:31 +1000

    project: reverted all code to master code

--- js/main.js
+++ js/main.js
@@ -1,67 +1,80 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function() {
 
 	// // Photo Filter
-	// let $btns = $('#portfolio .button-group button');
+	// let btns = document.querySelectorAll('#portfolio .button-group button');
 
-	// $btns.click(function(e){
+	// btns.forEach(function(btn) {
+	// 	btn.addEventListener('click', function(e) {
 
-	// 	$('#portfolio .button-group button').removeClass('active');
+	// 		document.querySelectorAll('#portfolio .button-group button').forEach(function(btn) {
+	// 			btn.classList.remove('active');
+	// 		});
 
-	// 	e.target.classList.add('active');
+	// 		e.target.classList.add('active');
 
+	// 		let selector = e.target.getAttribute('data-filter');
 
-	// 	let selector = $(e.target).attr('data-filter');
+	// 		document.querySelector('#portfolio .grid').isotope({
+	// 			filter: selector
+	// 		});
 
-	// 	$('#portfolio .grid').isotope({
-	// 		filter:selector
+	// 		return false;
 	// 	});
-
-	// 	return false;
 	// });
 
 	// // Photo Popup
-	// $('#portfolio .button-group #btn1').trigger('click');
+	// document.querySelector('#portfolio .button-group #btn1').click();
 
-	// $('#portfolio .grid .test-popup-link').magnificPopup({
-	// 	type: 'image',
-	// 	gallery:{enabled:true}
+	// let popupLinks = document.querySelectorAll('#portfolio .grid .test-popup-link');
+
+	// popupLinks.forEach(function(link) {
+	// 	link.addEventListener('click', function() {
+	// 		magnificPopup({
+	// 			type: 'image',
+	// 			gallery: {
+	// 				enabled: true
+	// 			}
+	// 		});
+	// 	});
 	// });
 
 
-	// //Owl Carousel
-	// $('#about-me .carousel-items .owl-carousel').owlCarousel({
-	// 	loop:true,
-	// 	dots:true,
-	// 	autoplay:true,
-	// 	responsive:{
-	// 		0:{
-	// 			items:1
-	// 		},
-	// 		544:{
-	// 			items:2
+	// // Owl Carousel
+	// let owl = document.querySelector('#about-me .carousel-items .owl-carousel');
+	// if (owl) {
+	// 	$(owl).owlCarousel({
+	// 		loop: true,
+	// 		dots: true,
+	// 		autoplay: true,
+	// 		responsive: {
+	// 			0: {
+	// 				items: 1
+	// 			},
+	// 			544: {
+	// 				items: 2
+	// 			}
 	// 		}
-	// 	}
-	// });
+	// 	});
+	// }
 
 	// AOS Instance
 	AOS.init();
 
-	//Navbar
-	let nav_offset_top=$('#header').height()-600;
-
-	function navbarFixed(){
-		if($('#header').length){
-			$(window).scroll(function(){
-				let scroll = $(window).scrollTop();
-
-				if(scroll >= nav_offset_top){
-					$('#header  .navigation').addClass('topbar');
-				}else{
-					$('#header .navigation').removeClass('topbar');
+	// Navbar
+	let header = document.querySelector('#header');
+	if (header) {
+		let nav_offset_top = header.offsetHeight - 600;
+		function navbarFixed() {
+			window.addEventListener('scroll', function() {
+				let scroll = window.pageYOffset;
+				if (scroll >= nav_offset_top) {
+					header.querySelector('.navigation').classList.add('topbar');
+				} else {
+					header.querySelector('.navigation').classList.remove('topbar');
 				}
 			});
 		}
+		navbarFixed();
 	}
-	navbarFixed();
 
 });
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,29 +1,39 @@
-(function($) {
-  "use strict"; // Start of use strict
+"use strict";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 55)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+document.querySelectorAll('a.js-scroll-trigger[href*="#"]:not([href="#"])').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = document.querySelector(this.hash);
+            target = target ? target : document.querySelector('[name=' + this.hash.slice(1) + ']');
+            if (target) {
+                e.preventDefault();
+                window.scrollTo({
+                    top: target.offsetTop - 55,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    });
+});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+document.querySelectorAll('.js-scroll-trigger').forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        document.querySelector('.navbar-collapse').classList.remove('show');
+    });
+});
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 100
-  });
-
-})(jQuery); // End of use strict
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    const mainNavLinks = document.querySelectorAll('#mainNav .nav-link');
+    mainNavLinks.forEach(link => {
+        const section = document.querySelector(link.hash);
+        if (
+            section.offsetTop - 56 <= currentScroll &&
+            section.offsetTop + section.offsetHeight - 56 > currentScroll
+        ) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+});
